c65ef4752686013885fdf522f3a714aff6bbb54a
user <dedihermawan102@gmail.com>
2025-09-08 21:25:53 +0700

    Program Pertama Saya di Kotlin

---
+++ src/Prak1BlokdanKomentar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, ini program pertama saya di kotlin")
+    //contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan Program
+     */
+}
